c831a86c29ae6b42dbdf820f5c4837221d505c35
Petr Knap <8299754+petrknap@users.noreply.github.com>
2026-01-07 22:56:11 +0100

    refactor: `TypedOptionalException` now extends root exception

--- src/Exception/TypedOptionalException.php
+++ src/Exception/TypedOptionalException.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace PetrKnap\Optional\Exception;
 
-/**
- * @todo extend {@see Exception}, not {@see OptionalException}
- */
-interface TypedOptionalException extends OptionalException
+interface TypedOptionalException extends Exception
 {
 }
